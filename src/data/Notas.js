2602d43c3d52862b60eefcf63b1aa907f5a9b6ec
Jonas Brito <jonasexplore@gmail.com>
2021-02-19 22:07:26 -0300

    Classes de dados.

---
+++ src/data/Notas.js
@@ -0,0 +1,34 @@
+export default class ArrayDeNotas {
+  constructor() {
+    this.notas = [];
+    this._inscritos = [];
+  }
+  inscrever(func) {
+    this._inscritos.push(func);
+  }
+  desinscrever(func) {
+    this._inscritos = this._inscritos.filter((f) => f !== func);
+  }
+  notificar() {
+    this._inscritos.forEach((func) => func(this.notas));
+  }
+
+  criarNota(titulo, desc, categoria) {
+    const novaNota = new Nota(titulo, desc, categoria);
+    this.notas.push(novaNota);
+    this.notificar();
+  }
+
+  deletarNotas(indice) {
+    this.notas.splice(indice, 1);
+    this.notificar();
+  }
+}
+
+class Nota {
+  constructor(titulo, desc, categoria) {
+    this.titulo = titulo;
+    this.desc = desc;
+    this.categoria = categoria;
+  }
+}
